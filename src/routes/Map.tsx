64b6b101e33ffda1558069d233ae29bef81491ec
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-14 22:13:31 +0900

    feat: Create Map Component

--- src/routes/Map.tsx
+++ src/routes/Map.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import styled from "styled-components";
+import { useQuery } from "react-query";
+import { getBoroughListFetch } from "../APIs/map";
+import Loading from "../components/common/Loading";
+import LeftMenu from "../components/Map/LeftMenu";
+import ThemeToggleButton from "../components/ThemeToggleButton";
+import NaverMap from "../components/Map/NaverMap";
+
+const Container = styled.main`
+  display: flex;
+`;
 
 function Map() {
-  return <div>Map</div>;
+  const { isLoading, data } = useQuery("getBoroughList", getBoroughListFetch);
+  function getLocation() {
+    if (navigator.geolocation) {
+      // GPS를 지원하면
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          alert(position.coords.latitude + " " + position.coords.longitude);
+        },
+        (error) => {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity,
+        }
+      );
+    } else {
+      alert("GPS를 지원하지 않습니다");
+    }
+  }
+  getLocation();
+
+  if (isLoading) return <Loading />;
+  else
+    return (
+      <Container>
+        <LeftMenu data={data} />
+        <NaverMap />
+        <ThemeToggleButton />
+      </Container>
+    );
 }
 
-export default Map;
+export default React.memo(Map);
