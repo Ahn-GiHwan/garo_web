5720e4f3cb3e4db78c61add5f5e7d7f60a15fc05
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-14 22:10:06 +0900

    feat: Create HomeButton component

---
+++ src/components/Buttons/HomeButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { NavIconButton } from "./style";
+import { useThemeState } from "../../context/ThemeState";
+
+function HomeButton() {
+  const { isDark } = useThemeState();
+
+  return (
+    <Link to="/" aria-label="Go Map Button">
+      <NavIconButton color="orange">
+        <AiFillHome size="30" color={isDark ? "white" : "black"} />
+      </NavIconButton>
+    </Link>
+  );
+}
+
+export default HomeButton;
