6fac9b8b45f053b724fe83c5efb421fbb184aff8
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-14 22:12:05 +0900

    feat: Create Home Component

---
+++ src/routes/Home.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import MapButton from "../components/Buttons/MapButton";
+import ChartButton from "../components/Buttons/ChartButton";
+import ExcelButton from "../components/Buttons/ExcelButton";
+
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  user-select: none;
+`;
+
+const Main = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 80%;
+  height: 300px;
+  padding-top: 80px;
+  border-radius: 10px;
+  background-color: #eeeeee;
+  color: black;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+const NavIcons = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 30%;
+`;
+
+function Home() {
+  return (
+    <Container>
+      <Main>
+        <Title>가로 휴지통</Title>
+        <NavIcons>
+          <MapButton />
+          <ChartButton />
+        </NavIcons>
+        <ExcelButton />
+      </Main>
+    </Container>
+  );
+}
+
+export default Home;
