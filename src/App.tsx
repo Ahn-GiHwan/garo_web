aa2d3c19dc773e6ed005673ca3f5722ce4bc3518
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-14 15:03:10 +0900

    feat: Import packages
    packages
    - react-router-dom
    - contextAPI
    - react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import AppInner from "./AppInner";
+import ContextProvider from "./context/ContextProvider";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <ContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <AppInner />
+        </QueryClientProvider>
+      </ContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
