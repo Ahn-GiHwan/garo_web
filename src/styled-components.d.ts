1837e0852798a28b6c85e9eef8df9a4f8ccac3ba
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-15 22:59:18 +0900

    type: Add subBg key

--- src/styled-components.d.ts
+++ src/styled-components.d.ts
@@ -4,14 +4,17 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     bg: string;
+    subBg: string;
     color: string;
   }
   export interface darkTheme {
     bg: string;
+    subBg: string;
     color: string;
   }
   export interface lightTheme {
     bg: string;
+    subBg: string;
     color: string;
   }
 }
