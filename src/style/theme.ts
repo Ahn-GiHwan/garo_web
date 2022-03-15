26c10de8ce98842d4a0bd822de4dbcc7a2f8df06
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-15 23:04:26 +0900

    style: Add subBg

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,9 +1,11 @@
 export const lightTheme = {
   bg: "#fff",
+  subBg: "#eeeeee",
   color: "black",
 };
 
 export const darkTheme = {
   bg: "#262422",
-  color: "white",
+  subBg: "#5a5656",
+  color: "#f1f1f1",
 };
