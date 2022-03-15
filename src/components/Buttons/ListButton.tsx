e62ddf30b91527a8af4dd9a47d7a5e1eb8c1565e
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-15 23:01:13 +0900

    feat: 리스트 오픈에 따라 아이콘 바뀜

--- src/components/Buttons/ListButton.tsx
+++ src/components/Buttons/ListButton.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { FaList } from "react-icons/fa";
 import { NavIconButton } from "./style";
 import { useThemeState } from "../../context/ThemeState";
+import { FaList } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 function ListButton({ isOpen, onClick }: { isOpen: boolean; onClick?: any }) {
   const { isDark } = useThemeState();
 
   return (
     <NavIconButton color="pink" select={isOpen} onClick={onClick}>
-      <FaList size="30" color={isDark ? "white" : "black"} />
+      {isOpen ? (
+        <AiOutlineClose size="30" color={isDark ? "white" : "black"} />
+      ) : (
+        <FaList size="30" color={isDark ? "white" : "black"} />
+      )}
     </NavIconButton>
   );
 }
