e9396d7dd28201df6fd3a0b1dbc826ccbe443be5
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-16 18:55:55 +0900

    feat: Add Marker of current location

--- src/components/Map/NaverMap.tsx
+++ src/components/Map/NaverMap.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 const { NaverMap: Map, Marker } = require("react-naver-maps");
 
-function NaverMap() {
+type MarkerLoc = {
+  lat: number;
+  lng: number;
+};
+
+function NaverMap({ lat, lng }: MarkerLoc) {
+  const navermaps = window.naver.maps;
+
   return (
     <Map
       style={{
         width: "100%",
         height: "100vh",
       }}
-      defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
-      defaultZoom={10}
-    />
+      defaultCenter={{ lat, lng }}
+      defaultZoom={14}
+      center={{ lat, lng }}
+    >
+      <Marker
+        position={new navermaps.LatLng(37.3595704, 127.105399)}
+        animation={0}
+        title="현재 위치"
+      />
+    </Map>
   );
 }
 
