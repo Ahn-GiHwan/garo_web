97ca9dccb7a60c95088e3c55c1fb812975c5fefd
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-16 19:56:35 +0900

    feat: 환경 설정 및 canvas모드 삭제

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,42 @@
+import { RenderAfterNavermapsLoaded } from "react-naver-maps";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import ContextProvider from "../src/context/ContextProvider";
+import { darkTheme, lightTheme } from "../src/style/theme";
+import GlobalStyles from "../src/style/GlobalStyles";
+
+export const decorators = [
+  (Story) => {
+    const isDark = false;
+    const queryClient = new QueryClient();
+    return (
+      <BrowserRouter>
+        <ContextProvider>
+          <QueryClientProvider client={queryClient}>
+            <RenderAfterNavermapsLoaded ncpClientId={process.env.REACT_APP_CLIENT_ID}>
+              <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+                <GlobalStyles />
+                <Story />
+              </ThemeProvider>
+            </RenderAfterNavermapsLoaded>
+          </QueryClientProvider>
+        </ContextProvider>
+      </BrowserRouter>
+    );
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewMode: "docs",
+  previewTabs: {
+    canvas: { hidden: true },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
